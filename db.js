c8dabef58894c1ff617d5adbe2674af70e274977
german gonzalez <gcomlnk@gmail.com>
2018-04-29 14:14:27 -0400

    updating db access woth mongoose

--- db.js
+++ db.js
@@ -1,17 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 const MongoClient = require('mongodb').MongoClient;
 const config = require('./src/config');
 
+
+
+const options = {
+    autoReconnect: true
+};
+
 module.exports = {
     connectTo: function (uri = config.mongo_uri) {
-        return MongoClient.connect(uri, function(err, db) {
-            if (err) {
-                throw err;
-            } else {
-                db.db(config.db_name);
-                console.log(`Successfully Connected to MongoDb: ${config.db_name}`);
-            }
-
-            db.close();
-        })
+        return mongoose.connect(`${uri}`, options)
+            .then(conn => console.log(`Connected to MongoDB DB:${config.db_name}`))
+            .catch(err => {
+                return err;
+            });
     },
 };
+
+
+// module.exports = {
+//     connectTo: function (uri = config.mongo_uri) {
+//         return MongoClient.connect(uri, function(err, db) {
+//             if (err) {
+//                 throw err;
+//             } else {
+//                 db.db(config.db_name);
+//                 console.log(`Successfully Connected to MongoDb: ${config.db_name}`);
+//             }
+//
+//             db.close();
+//         })
+//     },
+// };
